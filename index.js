a25a44622f6c0dca44173067a001684248962cab
Ankush Dutt <ankushd100@gmail.com>
2021-10-31 12:17:26 +0530

    Add DELETE route method

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.delete("/allocation/:id", (req, res) => {
+  res.send(
+    `Server: Got a DELETE request to delete lecture hall with id: ${req.params.id}`
+  );
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Running on PORT ${PORT}`);
