f1578e5f5899a334ace8632461bea6a124e34265
Ankush Dutt <ankushd100@gmail.com>
2021-10-31 23:54:37 +0530

    Setup MySQL

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import cors from "cors";
+import mysql from "mysql";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 app.use(cors());
+
+console.log(process.env.JAWSDB_URL);
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello");
