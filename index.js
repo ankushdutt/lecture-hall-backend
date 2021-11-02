f7628b806c9a1291b6a752468b5bda033ea5efbc
Ankush Dutt <ankushd100@gmail.com>
2021-11-02 15:33:54 +0530

    Add credentials parameter in cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
 
 console.log(process.env.JAWSDB_URL);
 var con = mysql.createConnection(process.env.JAWSDB_URL);
