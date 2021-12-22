6b5ea6e06e2a8d35dc7a70e8b68f5b088abba127
Ankush Dutt <ankushd100@gmail.com>
2021-12-23 03:14:25 +0530

    Fix auth errors

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-import express from "express";
-import cors from "cors";
-import mysql from "mysql";
-import dotenv from "dotenv";
-import passport from "passport";
-import LocalStrategy from "passport-local";
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 dotenv.config();
 const app = express();
-app.use(cors({ credentials: true, origin: true }));
 
+// Database Connection
 var db = mysql.createConnection(process.env.JAWSDB_URL);
 db.connect();
 
+// Middlewares
+app.use(cors({ credentials: true, origin: true }));
+app.use(express.json());
+app.use(passport.initialize());
+
 passport.use(
-  new LocalStrategy(function verify(email, password, cb) {
+  new LocalStrategy(function verify(username, password, cb) {
+    console.log("LocalStrategy ", username, password);
     db.query(
-      "SELECT * FROM user WHERE email = " +
-        email +
-        " AND password = " +
-        password,
-      function (err, result, fields) {
+      "SELECT * FROM user WHERE email = '" +
+        username +
+        "' AND password = '" +
+        password +
+        "';",
+      function (err, user, fields) {
         if (err) {
           return cb(err);
         }
-        if (!result) {
+        if (user.length === 0) {
           return cb(null, false, { message: "Incorrect email or password." });
         }
         return cb(null, user);
@@ -32,13 +39,29 @@ passport.use(
   })
 );
 
-app.post(
-  "/login",
-  passport.authenticate("local", { failureMessage: true }),
-  (req, res) => {
-    res.sendStatus(200);
-  }
-);
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
+});
+
+// Routes
+app.post("/login", function (req, res, next) {
+  passport.authenticate("local", function (err, user, info) {
+    console.log("req: ", req.body);
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(201).send("Incorrect email or password");
+    }
+    req.login(user, (err) => {
+      if (err) throw err;
+      res.status(200).send("Successfully Authenticated");
+    });
+  })(req, res, next);
+});
 
 app.get("/lecturehall/all", async function (req, res, next) {
   db.query("SELECT * FROM lecture_hall", function (err, result, fields) {
@@ -57,6 +80,7 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+// Listen
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
